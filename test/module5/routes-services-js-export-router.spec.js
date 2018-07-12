fb43a0b3110967fe392f9a680ebb75ba8c164149
Tom Bell <tjbell@gmail.com>
2018-07-12 00:44:31 -0600

    test fix

--- test/module5/routes-services-js-export-router.spec.js
+++ test/module5/routes-services-js-export-router.spec.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 describe('`services.js` exports', () => {
-  it('`services.js` should export router @routes-services-js-exports-router', () => {
+  it('`services.js` should export router @routes-services-js-export-router', () => {
     assert(typeof app === 'function', '`app` const has not been created in `app.js`.');
     assert(
       fs.existsSync(path.join(process.cwd(), 'src/routes/services.js')),
