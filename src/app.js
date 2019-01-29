e40866fcf8464be62c51d51623199c4c5ff961e3
zwaladeep <zwaladeep.sonapuram@gmail.com>
2019-01-29 13:36:50 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.set('views',path.join(__dirname,'views'));
 
 app.set('view engine','ejs');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname+'public')));
 
 app.get('/',(req,res) => res.render('index', {title: 'Index'}));
 
