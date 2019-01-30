9c9e643713809ead0466a22063dd3d9f539de6f5
zwaladeep <zwaladeep.sonapuram@gmail.com>
2019-01-30 09:42:12 +0530

    changes
    commit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
-
+app.use(express.urlencoded({extended: true}));
 
 const accountData = fs.readFileSync(
     path.join(__dirname,'json','accounts.json'),'utf8'
@@ -40,6 +40,36 @@ app.get('/credit',(req,res) => {
 app.get('/profile',(req,res) => {
     res.render('profile',{user: users[0]})
 });
+app.get('/transfer',(req,res)=>{
+    res.render('transfer')
+});
+
+app.post('/transfer',(req,res)=>{
+    accounts[req.body.from].balance= accounts[req.body.from].
+    balance- req.body.amount;
+    accounts[req.body.to].balance=
+    parseInt(accounts[req.body.to].balance)+
+    parseInt(req.body.amount,10);
+
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json',accountsJSON,'utf8'));
+res.render('transfer',{messsage:'transfer completed'});
+})
+app.get('/payment',(req,res)=>{
+    res.render('transfer')
+});
+app.post('/payment',(req,res)=>{
+
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount,10);
+const accountsJSON = JSON.stringify(accounts,null,4);
+fs.writeFileSync(path.join(__dirname,'json','accounts.json'),
+accountsJSON,'utf8');
+res.render('payment',{message: 'payment Successful',account: accounts.ceredit});
+})
+
+
+
 
 
 
